955636b6ef9d6df1bf5a3f21f1113a3205e4dc92
Caleb <cdonovi1@binghamton.edu>
2015-02-27 17:08:44 -0500

    Lab3

--- lab3/main.c
+++ lab3/main.c
@@ -1,12 +1,9 @@
 #include "standard.h"
 
-
-/*
- * Compile using the -lpthread flag
- */
 int main(int argc, char **argv) {
     pthread_t top_level_thread;
     args_c c;
+    int error;
     
     if (argc != 2) {
         usage();
@@ -24,23 +21,32 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+    if ((error = pthread_barrier_init(&barrier, NULL, (2 << c.level) - 1))) {
+        fprintf(stderr, "Error: pthread_barrier_init failed\nError %i\n", error);
+        return EXIT_FAILURE;
+    }
+
     c.threadNumber = 0;
 
-    /* note use of "&" when passing arguments */
-    if(pthread_create(&top_level_thread, NULL, create_thread, CAST_VOID(&c)) != 0) {
+    if(pthread_create(&top_level_thread, NULL, create_thread, CAST_VOID(&c))) {
         perror("pthread_create");
         return EXIT_FAILURE;
     }
 
-    if(pthread_join(top_level_thread, NULL) != 0) {
+    if(pthread_join(top_level_thread, NULL)) {
         perror("pthread_join");
         return EXIT_FAILURE;
     }
+    if ((error = pthread_barrier_destroy(&barrier))) {
+        fprintf(stderr, "Error: pthread_barrier_destroy failed\nError %i\n", error);
+        return EXIT_FAILURE;
+    }
+
 
     return EXIT_SUCCESS;
 }
 
-void* (create_thread)(void *args) {
+void* create_thread(void *args) {
     args_c *c =  CAST_C(args);
     fprintf(stdout, "Level %d, tid:  %ld\n", c->level, pthread_self());
     pthread_t new_thread1, new_thread2;
@@ -52,10 +58,12 @@ void* (create_thread)(void *args) {
         func = leaf_func;
 
         if ((arg1 = malloc(sizeof(args_l))) == NULL) {
-            //error
+            fprintf(stderr, "Error: problem with malloc, %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
         }
         if ((arg2 = malloc(sizeof(args_l))) == NULL) { 
-            //error
+            fprintf(stderr, "Error: problem with malloc, %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
         }
 
         CAST_L(arg1)->threadNumber = c->threadNumber*2 + 1;
@@ -64,8 +72,14 @@ void* (create_thread)(void *args) {
         /* make a new level of the tree */
         func = create_thread;
 
-        arg1 = malloc(sizeof(args_l));
-        arg2 = malloc(sizeof(args_l));
+        if ((arg1 = malloc(sizeof(args_c))) == NULL) {
+            fprintf(stderr, "Error: problem with malloc, %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        if ((arg2 = malloc(sizeof(args_c))) == NULL) { 
+            fprintf(stderr, "Error: problem with malloc, %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
 
         CAST_C(arg1)->threadNumber = c->threadNumber*2 + 1;
         CAST_C(arg2)->threadNumber = c->threadNumber*2 + 2;
@@ -73,25 +87,23 @@ void* (create_thread)(void *args) {
         CAST_C(arg2)->level = c->level - 1;
     }
 
-    if(pthread_create(&new_thread1, NULL, func, arg1) != 0) {
+    if(pthread_create(&new_thread1, NULL, func, arg1)) {
         perror("pthread_create");
         exit(EXIT_FAILURE);
     }
-    if(pthread_create(&new_thread2, NULL, func, arg2) != 0) {
+    if(pthread_create(&new_thread2, NULL, func, arg2)) {
         perror("pthread_create");
         exit(EXIT_FAILURE);
     }
+
     
-    for (int i = 0; i < 10; ++i) {
-        sched_yield();
-        fprintf(stdout, "Thread Number: %i, Thread ID: %ld, Index: %i\n", c->threadNumber, pthread_self(), i);
-    }
-    
-    if(pthread_join(new_thread1, NULL) != 0) {
+    busyWork(c->threadNumber);
+
+    if(pthread_join(new_thread1, NULL)) {
         perror("pthread_join");
         exit(EXIT_FAILURE);
     }
-    if(pthread_join(new_thread2, NULL) != 0) {
+    if(pthread_join(new_thread2, NULL)) {
         perror("pthread_join");
         exit(EXIT_FAILURE);
     }
@@ -100,7 +112,7 @@ void* (create_thread)(void *args) {
     free(arg1);
     free(arg2);
     
-    /* finish current thread (this function always succeeds) */
+    fprintf(stdout, "Thread %i is exiting\n", c->threadNumber); 
     pthread_exit(0);
 }
 
@@ -108,14 +120,7 @@ void* leaf_func(void *args) {
     args_l *l =  CAST_L(args);
 
     fprintf(stdout, "Level 0, tid:  %ld\n", pthread_self());
-
-    for (int i = 0; i < 10; ++i) {
-        sched_yield();
-        fprintf(stdout, "Thread Number: %i, Thread ID: %ld, Index: %i\n", l->threadNumber, pthread_self(), i);
-    }
-
-
-    /* finish current thread (this function always succeeds) */
+    busyWork(l->threadNumber);
     pthread_exit(0);
 }
 
@@ -130,5 +135,23 @@ static int parseStr(char *str, int *val) {
         return 0;
     } else {
         return -1;
+    }
+}
+
+static void busyWork(int TN) {
+    fprintf(stdout, "Thread %i has reached barrier\n", TN); 
+    int rc = pthread_barrier_wait(&barrier);
+    if (rc && rc != PTHREAD_BARRIER_SERIAL_THREAD) {
+        fprintf(stderr, "Error: pthread_barrier_wait failed\nError %i\n", rc);
+        exit(EXIT_FAILURE);
+
+    }
+
+
+    fprintf(stdout, "Thread %i has passed barrier\n", TN); 
+
+    for (int i = 0; i < 10; ++i) {
+        fprintf(stdout, "Thread Number: %i, Thread ID: %ld, Index: %i\n", TN, pthread_self(), i);
+        sched_yield();
     }
 }
--- lab3/standard.h
+++ lab3/standard.h
@@ -1,11 +1,8 @@
 #ifndef STANDARD_H
 #define STANDARD_H
 
-#define CAST_C(ptr) ((args_c *) (ptr))
-#define CAST_L(ptr) ((args_l *) (ptr))
-#define CAST_VOID(ptr) ((void *) (ptr))
-
 #include <pthread.h>
+#include <sys/types.h>
 #include <stdio.h> 
 #include <unistd.h>
 #include <stdbool.h>
@@ -14,6 +11,9 @@
 #include <errno.h>
 #include <string.h>
 
+#define CAST_C(ptr) ((args_c *) (ptr))
+#define CAST_L(ptr) ((args_l *) (ptr))
+#define CAST_VOID(ptr) ((void *) (ptr))
 
 typedef struct args_c {
     int level;
@@ -26,7 +26,7 @@ typedef struct args_l {
 
 static const char *USAGE_STRING = "Usage:\n./lab3 num-levels\n";
 static const int MAX_TREE_LEVELS = 5;
-static const int MIN_TREE_LEVELS = 0;
+static const int MIN_TREE_LEVELS = 1;
 
 static void usage(void);
 static int parseStr(char *str, int *val);
@@ -34,5 +34,7 @@ static int parseStr(char *str, int *val);
 void* create_thread(void *);
 void* leaf_func(void *);
 
+static void busyWork(int TN);
 
+pthread_barrier_t barrier;
 #endif
